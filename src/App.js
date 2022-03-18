eff10d23eb08febc8314c5701df584f17b0f03d7
Kaminyou <ikaminyou@gmail.com>
2022-03-18 21:22:50 +0800

    move title to config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Wheel from './containers/main';
+import configData from "./config.json";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>NTU Wheel of Dinner</h1>
+        <h1>{configData.TITLE}</h1>
         <Wheel />
       </header>
     </div>
